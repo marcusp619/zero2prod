d597e4f26d6b5c2787168d1f3d582a80f0df1ec8
marcusp619 <marcusp619@gmail.com>
2024-04-14 20:29:36 -0400

    refactor: move fn

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,6 +6,20 @@
 
 use std::net::TcpListener;
 
+// launch our application in the background ~somehow~
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let spawn = tokio::spawn(server);
+    std::mem::drop(spawn);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -24,18 +38,4 @@ async fn health_check_works() {
     // assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-// launch our application in the background ~somehow~
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let spawn = tokio::spawn(server);
-    std::mem::drop(spawn);
-    // We return the application address to the caller!
-    format!("http://127.0.0.1:{}", port)
 }
